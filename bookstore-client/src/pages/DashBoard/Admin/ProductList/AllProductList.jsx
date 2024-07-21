400442f35f3dfcd875cc3e9acc5624b6b6bc3ce0
Anh Toan <toan21420@gmail.com>
2024-07-22 00:24:48 +0700

    Upload source code

--- bookstore-client/src/pages/DashBoard/Admin/ProductList/AllProductList.jsx
+++ bookstore-client/src/pages/DashBoard/Admin/ProductList/AllProductList.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import { FaBook, FaClipboardList, FaPlus, FaRegEdit, FaUser } from "react-icons/fa";
+import {
+  FaBook,
+  FaClipboardList,
+  FaPlus,
+  FaRegEdit,
+  FaTrashAlt,
+  FaUser,
+  FaUserEdit,
+} from "react-icons/fa";
 import { MdLogout } from "react-icons/md";
 import { AiFillDashboard, AiOutlineBars } from "react-icons/ai";
 import "../DashBoard.css";
@@ -80,6 +88,47 @@ const AllProductList = () => {
                 <FaPlus></FaPlus>Thêm
               </button>
             </div>
+          </div>
+          <div className="mt-6 border rounded-[30px] p-5">
+            <table className="table w-full">
+              <thead className="text-[16px] font-semibold text-black">
+                <tr>
+                  <th>#</th>
+                  <th>Hình ảnh</th>
+                  <th>Tên sách</th>
+                  <th>Tác giả</th>
+                  <th>Danh mục</th>
+                  <th>Nhà xuất bản</th>
+                  <th>Giá</th>
+                  <th>Số lượng</th>
+                  <th className="text-center">Thao tác</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>
+                    <img src="./images/product.png" className="w-20 h-20" alt="" />
+                  </td>
+                  <td>Cây cam ngọt của tôi</td>
+                  <td>José Mauro de Vasconcelos</td>
+                  <td>Văn học</td>
+                  <td>Hội Nhà Văn</td>
+                  <td>199000đ</td>
+                  <td className="px-3 text-center">120 quyển</td>
+                  <td>
+                    <div className="flex items-center justify-center gap-3">
+                      <a href="#">
+                        <FaUserEdit className="w-5 h-5 text-main" />
+                      </a>
+                      <a href="#">
+                        <FaTrashAlt className="w-5 h-4 text-red" />
+                      </a>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
           {/* Content goes here */}
         </div>
